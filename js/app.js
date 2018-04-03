da548fc9a368248f80df000596a1cea5cb48bfeb
Emilia Walczak <emilia.walczak.87@wp.pl>
2018-04-03 22:06:36 +0200

    remove stars and adding reload function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-/*
- * Create a list that holds all of your cards:
- */
+//a list that holds all of your cards:
+
 let array= [
   'fa fa-diamond','fa fa-diamond',
   'fa fa-paper-plane-o','fa fa-paper-plane-o',
@@ -11,7 +10,8 @@ let array= [
   'fa fa-bicycle','fa fa-bicycle',
   'fa fa-bomb','fa fa-bomb',
 ]
-   //shuffle cards:
+
+//shuffle function:
    function shuffle(array) {  // Shuffle function from http://stackoverflow.com/a/2450976
        var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -25,38 +25,25 @@ let array= [
 
        return array;
    }
-array= shuffle(array); //shuffle the cards on the page
-/*
- * Display the cards on the page
-*/
-//for each method
-/*   - loop through each card and create its HTML
-*   - add each card's HTML to the page
-*/
-function addCards(item, index){
-  document.querySelector(".deck").innerHTML+=`<li class="card"><i class="${item}"></i></li>`
-}
+array= shuffle(array);
+
+//displays cards on the page:
+  function addCards(item, index)  {
+    document.querySelector(".deck").innerHTML+=`<li class="card"><i class="${item}"></i></li>`
+  }
 
 array.forEach(addCards);  //adding cards to html
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-let clicks = 0;
-$(".card").click(open);
- function open(e){
-   $(this).addClass("open show")
-   clicks= clicks+1;  //count clicks
- let openCards = document.getElementsByClassName("open","show");
+
+//open cards:
+  let clicks = 0;
+  $(".card").click(open);
+  function open(e){
+    $(this).addClass("open show")
+    clicks= clicks+1;  //count clicks
+    let openCards = document.getElementsByClassName("open","show");
  Array.from(openCards); //array of all open cards
 
-
+//matching cards:
 if (openCards.length>1) {
     if(openCards[0].firstChild.className === openCards[1].firstChild.className){
     openCards[0].classList.add("match");
@@ -72,8 +59,26 @@ if (openCards.length>1) {
   },700);
 }
 }  // turns the cards if they match or not turns if match
+
 //show clicks:
 document.querySelector(".moves").innerHTML=clicks;
-//display stars: //reload //timecount
 
+//display stars:
+if (clicks>20){
+  if (clicks<23){
+document.querySelector(".stars").lastChild.remove();
 }
+}
+if (clicks>40){
+  if (clicks<43){
+document.querySelector(".stars").lastChild.remove();
+}
+}
+}
+
+//reloading button:
+$(".restart").on("click", function(){
+  location.reload()
+})
+
+//
